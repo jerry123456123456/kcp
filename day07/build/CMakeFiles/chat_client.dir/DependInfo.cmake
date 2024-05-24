c85e9c62252bd90188b9f3a154134dd132de37a1
jerry <songqy2004@qq.com>
2024-05-24 12:08:37 +0800

    1

---
+++ day07/build/CMakeFiles/chat_client.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jerry/Documents/c++/day07/ikcp.c" "CMakeFiles/chat_client.dir/ikcp.c.o" "gcc" "CMakeFiles/chat_client.dir/ikcp.c.o.d"
+  "/home/jerry/Documents/c++/day07/chat_client.cc" "CMakeFiles/chat_client.dir/chat_client.cc.o" "gcc" "CMakeFiles/chat_client.dir/chat_client.cc.o.d"
+  "/home/jerry/Documents/c++/day07/kcp_client.cc" "CMakeFiles/chat_client.dir/kcp_client.cc.o" "gcc" "CMakeFiles/chat_client.dir/kcp_client.cc.o.d"
+  "/home/jerry/Documents/c++/day07/kcp_session.cc" "CMakeFiles/chat_client.dir/kcp_session.cc.o" "gcc" "CMakeFiles/chat_client.dir/kcp_session.cc.o.d"
+  "/home/jerry/Documents/c++/day07/udp_socket.cc" "CMakeFiles/chat_client.dir/udp_socket.cc.o" "gcc" "CMakeFiles/chat_client.dir/udp_socket.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ day07/build/CMakeFiles/kcp.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for kcp.
+# This may be replaced when dependencies are built.
