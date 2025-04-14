cc4bc39fba85c46493757f3678c7007719d845b5
jerry <songqy2004@qq.com>
2025-04-14 22:13:54 +0800

    1

--- day07/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jerry/Documents/c++/day07
-# Build directory: /home/jerry/Documents/c++/day07/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
